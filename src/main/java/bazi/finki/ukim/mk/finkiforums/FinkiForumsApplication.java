9a16338b584a7d1ac16bb17c74d294792d23cb93
LinaNakova <lina.nakova@gmail.com>
2022-12-26 13:33:57 +0100

    Initial commit

---
+++ src/main/java/bazi/finki/ukim/mk/finkiforums/FinkiForumsApplication.java
@@ -0,0 +1,13 @@
+package bazi.finki.ukim.mk.finkiforums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinkiForumsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinkiForumsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bazi/finki/ukim/mk/finkiforums/FinkiForumsApplicationTests.java
@@ -0,0 +1,13 @@
+package bazi.finki.ukim.mk.finkiforums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinkiForumsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
